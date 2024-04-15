143520ea6d3b3e6c2f4f5a8882f7abeefda5a532
Blackpossum <yosaphatharwindra@gmail.com>
2024-04-15 16:48:40 +0700

    adding and configuring dependencies

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Blackpossum/go_backend_practice
 
 go 1.22.2
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello World!")
+	// fmt.Println("hello World!")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("ports incorectlly set in env")
+	}
+	// make a router using chi package
+	router := chi.NewRouter()
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	// start the server and capture error if any
+	log.Printf("server running on port %v", portString)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
