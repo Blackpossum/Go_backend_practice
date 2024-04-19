889520c453689f7568539e2f4b129d43522afc10
blackpossum <133690316+Blackpossum@users.noreply.github.com>
2024-04-20 02:08:29 +0700

    tweaks handler_error.go
    add another description

--- handler_error.go
+++ handler_error.go
@@ -5,5 +5,5 @@ import "net/http"
 // this is the http handler respoonse if succesfull
 
 func Handlerr_Error(w http.ResponseWriter, r *http.Request) {
-	RespondWithError(w, http.StatusBadRequest, "Invalid request, something went wrong")
+	RespondWithError(w, http.StatusBadRequest, "Invalid request, something went wrong or the server is down")
 }
